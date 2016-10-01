ac650608d2ed554e1d640ccc25b017dda96ff2cf
Emily Bertelson <emily.bertelson@gmail.com>
2016-09-30 22:50:42 -0600

    view is broken, but deserialization is happening now

--- src/main/java/com/emilybertelson/newtonmovies/controller/SearchController.java
+++ src/main/java/com/emilybertelson/newtonmovies/controller/SearchController.java
@@ -1,5 +1,7 @@
 package com.emilybertelson.newtonmovies.controller;
 
+import com.google.gson.*;
+import com.google.gson.annotations.SerializedName;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,21 +9,82 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.lang.reflect.Type;
+
+
+class Movie {
+    @SerializedName("Title")
+    private String title;
+    @SerializedName("Year")
+    private String year;
+    @SerializedName("imdbID")
+    private String imdbID;
+
+    public Movie() { }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+}
+
+class MoviesCollection {
+    @SerializedName("Search")
+    public Movie[] movies;
+
+    public MoviesCollection() { }
+}
+
 @Controller
 public class SearchController {
 
     @RequestMapping("/search")
-    public ModelAndView showMessage(
-            /*@RequestParam(value = "name", required = false, defaultValue = "World") String name*/) {
+    public ModelAndView showResult(
+            @RequestParam(value = "title", required = false, defaultValue = "Newton") String title) {
 
         // make the HTTP request to OMdb
         RestTemplate restTemplate = new RestTemplate();
         String response = restTemplate.getForObject(
-                "http://www.omdbapi.com/?s=newton&type=movie&r=json",
+                "http://www.omdbapi.com/?s=" + title + "&type=movie&r=json",
                 String.class);
 
+        Gson g = new GsonBuilder().create();
+
+        MoviesCollection moviesCollection = g.fromJson(response, MoviesCollection.class);
+
+        String tableRows = "";
+        for(int i = 0; i < moviesCollection.movies.length; i++)
+        {
+            tableRows += "<tr>";
+            tableRows += "<td>" + moviesCollection.movies[i].getTitle() + "</td>";
+            tableRows += "<td>" + moviesCollection.movies[i].getYear() + "</td>";
+            tableRows += "<td><a href=\"http://imdb.com/title/" + moviesCollection.movies[i].getImdbID() + "/\">IMDb</a></td>";
+            tableRows += "</tr>";
+        }
+
         ModelAndView mv = new ModelAndView("search");
-        mv.addObject("json", response);
+        mv.addObject("title", title);
+        mv.addObject("movies", moviesCollection.movies);
+        mv.addObject("tableRows", tableRows);
         return mv;
     }
 }
--- src/main/webapp/WEB-INF/views/search.jsp
+++ src/main/webapp/WEB-INF/views/search.jsp
@@ -1,16 +1,27 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Results - Find movies that have "Newton" in the title</title>
+<title>Results - Find movies that have "${title}" in the title</title>
 </head>
 <body>
 	<center>
-		<h1>Movies that have "Newton" in the title:</h1>
-		<div>
-			${json}
-		</div>
+		<h1>Movies that have "${title}" in the title:</h1>
+		<table>
+			${tableRows}
+		</table>
+		<table>
+			<c:forEach var="m" items="${movies}">
+				<tr>
+					<td>${m.title}</td>
+					<td>${m.year}</td>
+					<td><a href="http://www.imdb.com/title/${m.imdbID}/">IMDb</a></td>
+				</tr>
+			</c:forEach>
+		</table>
+
 	</center>
 </body>
 </html>
